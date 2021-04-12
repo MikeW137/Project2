0fc1d5bdbf1049aa38cc3ae2eecd07d8e3462cae
Michael Wydra <wydrama@gmail.com>
2021-04-12 11:02:54 -0500

    added author class

---
+++ src/main/java/com/bookstore/demo/model/Author.java
@@ -0,0 +1,93 @@
+package com.bookstore.demo.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="authors")
+public class Author {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String nationality;
+
+    @Column
+    private Integer age;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+
+    public Author() {
+    }
+
+    public Author(Long id, String name, String nationality, Integer age, Book book) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.age = age;
+        this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", age=" + age +
+                ", book=" + book +
+                '}';
+    }
+}
