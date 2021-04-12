3275da4f5c9d2e6858f8a239f9171ba5d2681f49
Hristo Papucharov <hpapucharov@gmail.com>
2021-04-12 09:44:10 -0500

    Controller/Service 1

--- src/main/java/com/bookstore/demo/controller/BookController.java
+++ src/main/java/com/bookstore/demo/controller/BookController.java
@@ -1,4 +1,30 @@
 package com.bookstore.demo.controller;
 
+import com.bookstore.demo.model.Book;
+import com.bookstore.demo.repository.BookRepository;
+import com.bookstore.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
 public class BookController {
+    private BookService bookService;
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Autowired
+    public void setBookService(BookService bookService) {
+        this.bookService = bookService;
+    }
+
 }
+
+
+
+
---
+++ src/main/java/com/bookstore/demo/service/BookService.java
@@ -0,0 +1,16 @@
+package com.bookstore.demo.service;
+
+import com.bookstore.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+}
