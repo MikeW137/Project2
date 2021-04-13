73e881800ca0ba14351e0954165f0e719a176cd0
Hristo Papucharov <hpapucharov@gmail.com>
2021-04-13 11:25:40 -0500

    Author JUnit Test

--- src/test/java/com/bookstore/demo/BookstoreApplicationTests.java
+++ src/test/java/com/bookstore/demo/BookstoreApplicationTests.java
@@ -2,6 +2,7 @@ package com.bookstore.demo;
 
 import com.bookstore.demo.model.Author;
 import com.bookstore.demo.model.Book;
+import com.bookstore.demo.repository.AuthorRepository;
 import com.bookstore.demo.repository.BookRepository;
 import com.bookstore.demo.service.BookService;
 import org.junit.jupiter.api.DisplayName;
@@ -18,9 +19,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BookstoreApplicationTests {
 
     private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
     @Autowired
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+    @Autowired
+    public void setAuthorRepository(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     @Test
@@ -28,15 +34,18 @@ class BookstoreApplicationTests {
     }
 
     @Test
-    @DisplayName("Ensure correct handling of zero")
+
     public void testGetBook(){
         Book book = new Book(1L, "Book One", "Book One Description");
         Book found = bookRepository.findByTitle("Book One");
         assertEquals(book.toString(), found.toString());
     }
-    
-    public void testGetAuthor(){
 
+    @Test
+    public void testGetAuthor(){
+        Author author = new Author(2L, "Marc", "USA", 32, new Book(1L, "Book One", "Book One Description"));
+        List<Author> found = authorRepository.findByBookId(1L);
+        assertEquals(author.toString(), found.get(0).toString());
     }
 
 }
