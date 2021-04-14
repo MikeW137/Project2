084934d33a38c3210e82dc5dee315ba2f9856634
Hristo Papucharov <hpapucharov@gmail.com>
2021-04-14 09:42:49 -0500

    MyUserDetails/MyUserDetailsService

--- src/main/java/com/bookstore/demo/security/MyUserDetailsService.java
+++ src/main/java/com/bookstore/demo/security/MyUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.bookstore.demo.security;
 
-public class MyUserDetailsService {
+import com.bookstore.demo.model.User;
+import com.bookstore.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
 }
