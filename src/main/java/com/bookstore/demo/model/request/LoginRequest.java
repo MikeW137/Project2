7ecce3441870a8beca478fc001efe63c0bf0dffd
Hristo Papucharov <hpapucharov@gmail.com>
2021-04-14 09:46:31 -0500

    LoginRequest

--- src/main/java/com/bookstore/demo/model/request/LoginRequest.java
+++ src/main/java/com/bookstore/demo/model/request/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.bookstore.demo.model.request;
 
 public class LoginRequest {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
